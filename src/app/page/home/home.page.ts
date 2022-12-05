62e8610c4f1943a1a503bffad4b6b7d49f2393da
Thyago <thyagoalmeidaprof@gmail.com>
2022-12-04 23:06:04 -0300

    V_002

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,60 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  image = "https://cdn.pixabay.com/photo/2016/05/16/17/59/strawberries-1396330__340.jpg";
+  products = [
+    {
+      product: 'Macarrão', quant: '2k'
+    },
+    {
+      product: 'Feijão', quant: '2k'
+    },
+    {
+      product: 'Batata', quant: '2k'
+    },
+    {
+      product: 'Cenoura', quant: '2k'
+    }
+  ]
+  constructor(
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController,
+    private loadCtrl: LoadingController
+    ) {}
+
+    ngOnInit(){
+      this.load();
+    }
+
+  async deletar(){
+   
+    const alert = this.alertCtrl.create({
+      mode: 'ios',
+      message: 'Item Excluido',
+      buttons: ['OK'],
+    });
+    (await alert).present();
+  }
+
+  async toast(){
+    const toastMessage = this.toastCtrl.create({
+      mode: 'ios',
+      header: 'Alteração de Produtos',
+      duration: 2000,
+    });
+
+    (await toastMessage).present();
+  }
+
+  async load(){
+    const loadingct = this.loadCtrl.create({
+      mode:'ios',
+      message: 'Aguarde Carregando',
+      duration: 2000
+    });
+    (await loadingct).present();
+  }
+
+
 
 }
