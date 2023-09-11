3899ec4868b6161eabb776fe3bda0cf0ace93a60
Thyago <thyagoalmeidaprof@gmail.com>
2023-09-11 18:54:01 -0300

    ProjetoBlank

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./page/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
 
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'botoes',
-    loadChildren: () => import('./botoes/botoes.module').then( m => m.BotoesPageModule)
-  },
-  {
-    path: 'alerta',
-    loadChildren: () => import('./alerta/alerta.module').then( m => m.AlertaPageModule)
-  },
-  {
-    path: 'actionsheet',
-    loadChildren: () => import('./actionsheet/actionsheet.module').then( m => m.ActionsheetPageModule)
   }
   
 ];
